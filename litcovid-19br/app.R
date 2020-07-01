2dcf473a1732903210272cea481f014dd6cf1cb3
André Alves <42812893+andrealves19@users.noreply.github.com>
2020-07-01 17:03:19 -0300

    inital commit

---
+++ litcovid-19br/app.R
@@ -0,0 +1,27 @@
+
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- htmlTemplate(
+    filename = "www/index.html",
+    plotOutput = plotOutput(outputId = "distPlot")
+
+)
+
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x))
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
