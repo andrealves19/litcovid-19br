0ab137d0df848f887e1df44ccf9af974245ca46d
André Alves <42812893+andrealves19@users.noreply.github.com>
2020-07-02 01:47:57 -0300

    add highcharts/maps

--- litcovid-19br/app.R
+++ litcovid-19br/app.R
@@ -1,28 +1,49 @@
+# Packages 
 
+require(shiny)
+require(ggplot2)
+require(highcharter)
+require(forecast)
+require(htmlwidgets)
+require(shinyjs)
+require(shinyWidgets)
 
-library(shiny)
-library(ggplot2)
-
-# Define UI for application that draws a histogram
+# Define UI for application 
 ui <- htmlTemplate(
     filename = "www/index.html",
-    plotOutput = plotOutput(outputId = "distPlot")
+    plotOutput = highchartOutput(outputId = "hcontainer"),
+    mapOutput = highchartOutput(outputId = "mapcontainer")
 
 )
 
 
-# Define server logic required to draw a histogram
+# Define server logic required to draw a timeseries
 server <- function(input, output) {
+    
+    
+    output$hcontainer <- renderHighchart({
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x))
-
-        # draw the histogram with the specified number of bins
-        ggplot(faithful, aes(x = eruptions, y = waiting)) + 
-            geom_point() +
-            theme_bw()
+        airforecast <- forecast(auto.arima(AirPassengers), level = 95)
+        
+        hchart(airforecast)
+    })
+    
+    
+    output$mapcontainer <- renderHighchart({
+        
+        mapdata <- get_data_from_map(download_map_data("countries/us/us-all"))
+        
+        data_fake <- mapdata %>% 
+            select(code = `hc-a2`) %>% 
+            mutate(value = 1e5 * abs(rt(nrow(.), df = 10)))
+        
+        hcmap("countries/us/us-all", data = data_fake, value = "value",
+              joinBy = c("hc-a2", "code"), name = "Fake data",
+              dataLabels = list(enabled = TRUE, format = '{point.name}'),
+              borderColor = "#FAFAFA", borderWidth = 0.1,
+              tooltip = list(valueDecimals = 2, valuePrefix = "$", valueSuffix = " USD")) 
+        
+        
     })
 }
 
